2e4dd2d7896845d5a80b8529adaa6a1df3a9da5a
Capybaric123 <dancho321sorokin@gmail.com>
2026-08-01 11:55:26 -0400

    I never was this close to smashing my monitor

--- src/main.c
+++ src/main.c
@@ -120,19 +120,23 @@ int main(void)
     }
 
     // List of vertices for the rectangle
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+    GLfloat vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     // Create the VAO and the VBO
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // Bind the VAO before the VBO
     glBindVertexArray(VAO);
@@ -141,6 +145,9 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // Set the vertex attribute pointer
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -148,6 +155,9 @@ int main(void)
     // Bind the vertex buffer (VBO)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    glBindVertexArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     // Set the initial window size OpenGL viewport
     glViewport(0, 0, 640, 480);
 
@@ -187,8 +197,11 @@ int main(void)
         // Use the program and bind the VAO
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+
         // Draw call
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
 
         // Make the window not show up as not responding
         glfwPollEvents();
@@ -198,6 +211,7 @@ int main(void)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     // Destroy the window on exit
     glfwDestroyWindow(window);
